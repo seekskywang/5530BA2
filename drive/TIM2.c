beded0ca3779e6a7398085da9891d25ae7b7b169
seekskywang <seekskywang@gmail.com>
2019-05-30 16:38:44 +0800

    1

--- drive/TIM2.c
+++ drive/TIM2.c
@@ -193,12 +193,12 @@ void TIM2_PWM_Config(void)
 /**************************************************************************************/
 void Temp_Comapre(void)	  //温度来控制风扇
 {
-	if(NTC_value<=1500 || NTC_value > 3200)
+	if(NTC_value<=2450 || NTC_value > 5000)
 	{
 		GPIO_SetBits(GPIOE,GPIO_Pin_6);//开启风扇
 		flag_FAN_ON=1;
 	}
-	else if(NTC_value>=1600 && NTC_value <=3000)
+	else if(NTC_value>=2600 && NTC_value <=5000)
 	{
 		GPIO_ResetBits(GPIOE,GPIO_Pin_6);//关闭风扇
 		flag_FAN_ON=0;
